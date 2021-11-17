57cdc60fbc372c91c9edff3a456e5641651d0a82
Julio Cesar <czar.santos@outlook.com>
2021-11-17 20:43:46 -0300

    Formulários PHP

--- js/script.js
+++ js/script.js
@@ -28,3 +28,15 @@ if(window.SimpleAnime){
 new SimpleAnime();
 
 }
+
+
+
+if(window.SimpleForm){
+//Inicia msg Formulario PHP
+new SimpleForm({
+  form: ".formphp", // seletor do formulário
+  button: "#enviar", // seletor do botão
+  erro: "<div id='form-erro'><h2>Erro ao enviar</h2><p>Um erro ocorreu, tente para o email contato@bikcraft.com.</p></div>", // mensagem de erro
+  sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
+});
+}
